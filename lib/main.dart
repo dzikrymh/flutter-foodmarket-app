640b806af562627e9ce404d2e2a1db8bb709773a
Dzikry Maulana Hakim <dzikry.maulana.hakim@gmail.com>
2021-02-15 11:58:09 +0700

    part5 - Create Custom Bottom NavBar

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-        debugShowCheckedModeBanner: false, home: SignInPage());
+    return GetMaterialApp(debugShowCheckedModeBanner: false, home: MainPage());
   }
 }
