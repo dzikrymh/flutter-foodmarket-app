4b9ee1faabd216c27bea7bfa90f2d500e5fdc98f
Dzikry Maulana Hakim <dzikry.maulana.hakim@gmail.com>
2021-02-17 20:26:47 +0700

    part11 - foods list item

--- lib/models/food.dart
+++ lib/models/food.dart
@@ -23,31 +23,51 @@ List<Food> mockFood = [
   Food(
       id: 1,
       picturePath:
-          'https://cdns.klimg.com/merdeka.com/i/w/news/2020/11/03/1238499/content_images/670x335/20201103203146-1-sensasi-makan-ala-sultan-kulineran-beda-negara-dalam-sekali-tunjuk-saja-001-zaki.jpg',
-      name: 'Sate Sayur Sultan',
+          "https://i.pinimg.com/736x/06/7b/28/067b2879e5c9c42ec669bf639c3fbffc.jpg",
+      name: "Sate Sayur Sultan",
       description:
-          'Sate Sayur Sultan adalah menu sate vegan paing terkenal di Bandung. Sate ini di buat dari berbagai bahan.',
-      ingredients: 'Bawang Merah, Paprika, Bawang Bombay, Timun',
+          "Sate Sayur Sultan adalah menu sate vegan paling terkenal di Bandung. Sate ini dibuat dari berbagai macam bahan bermutu tinggi. Semua bahan ditanam dengan menggunakan teknologi masa kini sehingga memiliki nutrisi yang kaya.",
+      ingredients: "Bawang Merah, Paprika, Bawang Bombay, Timun",
       price: 150000,
-      rate: 4.9),
+      rate: 4.2),
   Food(
       id: 2,
       picturePath:
-          'https://cdns.klimg.com/merdeka.com/i/w/news/2020/11/03/1238499/content_images/670x335/20201103203146-1-sensasi-makan-ala-sultan-kulineran-beda-negara-dalam-sekali-tunjuk-saja-001-zaki.jpg',
-      name: 'Sate Sayur Sultan',
+          "https://cdns.klimg.com/dream.co.id/resources/news/2020/04/06/133546/bikin-steak-di-rumah-pastikan-bumbunya-meresap-2004066.jpg",
+      name: "Steak Daging Sapi Korea",
       description:
-          'Sate Sayur Sultan adalah menu sate vegan paing terkenal di Bandung. Sate ini di buat dari berbagai bahan.',
-      ingredients: 'Bawang Merah, Paprika, Bawang Bombay, Timun',
-      price: 150000,
+          "Daging sapi Korea adalah jenis sapi paling premium di Korea. Namun, untuk menikmatinya Anda tidak perlu jauh-jauh ke Korea Selatan. Steak Sapi Korea Oppa Bandung ini sudah terkenal di seluruh Indonesia dan sudah memiliki lebih dari 2 juta cabang.",
+      ingredients: "Daging Sapi Korea, Garam, Lada Hitam",
+      price: 750000,
       rate: 4.5),
   Food(
       id: 3,
       picturePath:
-          'https://cdns.klimg.com/merdeka.com/i/w/news/2020/11/03/1238499/content_images/670x335/20201103203146-1-sensasi-makan-ala-sultan-kulineran-beda-negara-dalam-sekali-tunjuk-saja-001-zaki.jpg',
-      name: 'Sate Sayur Sultan',
+          "https://i1.wp.com/varminz.com/wp-content/uploads/2019/12/mexican-chopped-salad3.jpg?fit=843%2C843&ssl=1",
+      name: "Mexican Chopped Salad",
       description:
-          'Sate Sayur Sultan adalah menu sate vegan paing terkenal di Bandung. Sate ini di buat dari berbagai bahan.',
-      ingredients: 'Bawang Merah, Paprika, Bawang Bombay, Timun',
-      price: 150000,
-      rate: 3.7)
+          "Salad ala mexico yang kaya akan serat dan vitamin. Seluruh bahan diambil dari Mexico sehingga akan memiliki cita rasa yang original.",
+      ingredients: "Jagung, Selada, Tomat Ceri, Keju, Wortel",
+      price: 105900,
+      rate: 3.9),
+  Food(
+      id: 4,
+      picturePath:
+          "https://images.immediate.co.uk/production/volatile/sites/2/2016/08/25097.jpg?quality=90&resize=768,574",
+      name: "Sup Wortel Pedas",
+      description:
+          "Sup wortel pedas yang unik ini cocok banget buat kalian-kalian yang suka pedas namun ingin tetap sehat. Rasanya yang unik akan memanjakan lidah Anda.",
+      ingredients: "Wortel, Seledri, Kacang Tanah, Labu, Garam, Gula",
+      price: 60000,
+      rate: 4.9),
+  Food(
+      id: 5,
+      picturePath:
+          "https://cmxpv89733.i.lithium.com/t5/image/serverpage/image-id/478345i84598AB4FEB454CB/image-size/large?v=1.0&px=999",
+      name: "Korean Raw Beef Tartare",
+      description:
+          "Daging sapi Korea cincang yang disajikan mentah dan disiram saus spesial dengan toping kuning telur dan taburan biji wijen.",
+      ingredients: "Daging Sapi Korea, Telur, Biji Wijen",
+      price: 350000,
+      rate: 3.4)
 ];
