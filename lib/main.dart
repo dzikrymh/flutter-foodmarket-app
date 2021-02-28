53a3db32d3aa111d61d3d053020411b881f27ef8
Dzikry Maulana Hakim <dzikry.maulana.hakim@gmail.com>
2021-02-28 13:01:02 +0700

    part13 - ui payment

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
         debugShowCheckedModeBanner: false,
-        home: FoodDetailPage(
-          transaction: Transaction(food: mockFood[0]),
+        home: PaymentPage(
+          transaction: Transaction(
+              food: mockFood[0],
+              user: mockUser,
+              quantity: 2,
+              total: (mockFood[0].price * 2 * 1.1).toInt() + 50000),
         ));
   }
 }
