5b98aa352a3993e27f52830266a389d904e04b2d
Dzikry Maulana Hakim <dzikry.maulana.hakim@gmail.com>
2021-03-19 14:40:09 +0700

    Sign In Service Process

--- lib/services/user_services.dart
+++ lib/services/user_services.dart
@@ -1,12 +1,29 @@
 part of 'services.dart';
 
 class UserServices {
-  static Future<ApiReturnValue<User>> signIn(
-      String email, String password) async {
-    await Future.delayed(Duration(milliseconds: 500));
+  static Future<ApiReturnValue<User>> signIn(String email, String password,
+      {http.Client client}) async {
+    if (client == null) {
+      client = http.Client();
+    }
 
-    return ApiReturnValue(value: mockUser);
-    // return ApiReturnValue(message: "Wrong email or password");
+    String url = baseURL + 'login';
+
+    var response = await client.post(url,
+        headers: {'Content-Type': 'application/json'},
+        body:
+            jsonEncode(<String, String>{'email': email, 'password': password}));
+
+    if (response.statusCode != 200) {
+      return ApiReturnValue(message: 'Please try again');
+    }
+
+    var data = jsonDecode(response.body);
+
+    User.token = data['data']['access_token'];
+    User value = User.fromJson(data['data']['user']);
+
+    return ApiReturnValue(value: value);
   }
 
   static Future<ApiReturnValue<User>> signUp(User user, String password,
--- lib/ui/pages/sign_in_page.dart
+++ lib/ui/pages/sign_in_page.dart
@@ -56,6 +56,7 @@ class _SignInPageState extends State<SignInPage> {
                 borderRadius: BorderRadius.circular(8),
                 border: Border.all(color: Colors.black)),
             child: TextField(
+              obscureText: true,
               controller: passwordController,
               decoration: InputDecoration(
                   border: InputBorder.none,
@@ -71,51 +72,51 @@ class _SignInPageState extends State<SignInPage> {
             child: isLoading
                 ? loadingIndicator
                 : RaisedButton(
-              onPressed: () async {
-                setState(() {
-                  isLoading = true;
-                });
+                    onPressed: () async {
+                      setState(() {
+                        isLoading = true;
+                      });
 
-                await context.bloc<UserCubit>().signIn(
-                    emailController.text, passwordController.text);
-                UserState state = context.bloc<UserCubit>().state;
+                      await context.bloc<UserCubit>().signIn(
+                          emailController.text, passwordController.text);
+                      UserState state = context.bloc<UserCubit>().state;
 
-                if (state is UserLoaded) {
-                  context.bloc<FoodCubit>().getFoods();
-                  context.bloc<TransactionCubit>().getTransactions();
-                  Get.to(MainPage());
-                } else {
-                  Get.snackbar("", "",
-                      backgroundColor: "D9435E".toColor(),
-                      icon: Icon(
-                        MdiIcons.closeCircleOutline,
-                        color: Colors.white,
-                      ),
-                      titleText: Text(
-                        "Sign In Failed",
-                        style: GoogleFonts.poppins(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w600),
-                      ),
-                      messageText: Text(
-                        (state as UserLoadingFailed).message,
-                        style: GoogleFonts.poppins(color: Colors.white),
-                      ));
-                  setState(() {
-                    isLoading = false;
-                  });
-                }
-              },
-              elevation: 0,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8)),
-              color: mainColor,
-              child: Text(
-                'Sign In',
-                style: GoogleFonts.poppins(
-                    color: Colors.black, fontWeight: FontWeight.w500),
-              ),
-            ),
+                      if (state is UserLoaded) {
+                        context.bloc<FoodCubit>().getFoods();
+                        context.bloc<TransactionCubit>().getTransactions();
+                        Get.to(MainPage());
+                      } else {
+                        Get.snackbar("", "",
+                            backgroundColor: "D9435E".toColor(),
+                            icon: Icon(
+                              MdiIcons.closeCircleOutline,
+                              color: Colors.white,
+                            ),
+                            titleText: Text(
+                              "Sign In Failed",
+                              style: GoogleFonts.poppins(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                            messageText: Text(
+                              (state as UserLoadingFailed).message,
+                              style: GoogleFonts.poppins(color: Colors.white),
+                            ));
+                        setState(() {
+                          isLoading = false;
+                        });
+                      }
+                    },
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    color: mainColor,
+                    child: Text(
+                      'Sign In',
+                      style: GoogleFonts.poppins(
+                          color: Colors.black, fontWeight: FontWeight.w500),
+                    ),
+                  ),
           ),
           Container(
             width: double.infinity,
@@ -124,23 +125,23 @@ class _SignInPageState extends State<SignInPage> {
             padding: EdgeInsets.symmetric(horizontal: defaultMargin),
             child: isLoading
                 ? SpinKitFadingCircle(
-              size: 45,
-              color: mainColor,
-            )
+                    size: 45,
+                    color: mainColor,
+                  )
                 : RaisedButton(
-              onPressed: () {
-                Get.to(SignUpPage());
-              },
-              elevation: 0,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8)),
-              color: grayColor,
-              child: Text(
-                'Create New Account',
-                style: GoogleFonts.poppins(
-                    color: Colors.white, fontWeight: FontWeight.w500),
-              ),
-            ),
+                    onPressed: () {
+                      Get.to(SignUpPage());
+                    },
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    color: grayColor,
+                    child: Text(
+                      'Create New Account',
+                      style: GoogleFonts.poppins(
+                          color: Colors.white, fontWeight: FontWeight.w500),
+                    ),
+                  ),
           )
         ],
       ),
